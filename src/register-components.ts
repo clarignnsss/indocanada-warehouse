416be03f9c5f35f0e49e6b575a363c7e0f7db0ff
Steven Albert <steven.albert@biac.co.id>
2023-03-23 17:01:25 +0700

    Rearrange register primevue component

--- src/register-components.ts
+++ src/register-components.ts
@@ -8,19 +8,22 @@ import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
 import PrimeVue from 'primevue/config'
+
 import Button from 'primevue/button'
-import Sidebar from 'primevue/sidebar'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
 import Card from 'primevue/card'
+import Column from 'primevue/column'
+import DataTable from 'primevue/datatable'
+import InputText from 'primevue/inputtext'
+import Sidebar from 'primevue/sidebar'
 
 export function registerPrimeVue(app: App) {
   app.use(PrimeVue)
   
   // Register components
   app.component('Button', Button)
-  app.component('Sidebar', Sidebar)
-  app.component('DataTable', DataTable)
-  app.component('Column', Column)
   app.component('Card', Card)
+  app.component('Column', Column)
+  app.component('DataTable', DataTable)
+  app.component('InputText', InputText)
+  app.component('Sidebar', Sidebar)
 }
