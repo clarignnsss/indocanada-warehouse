244528d38673e183154db8b94d20bebe1d81cbf6
clarignnsss <reginanaclarissa@gmail.com>
2023-05-16 10:28:48 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,16 @@
 <template>
-  <swiper
-    :direction="'vertical'"
-    :pagination="{
-      clickable: true,
-    }"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
-  </swiper>
+  <div id="app">
+    <router-view/>
+    <Footer />
+  </div>
 </template>
+
 <script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue'
+import Footer from "@/components/Footer.vue"
 
-  // Import Swiper styles
-  import 'swiper/css'
-
-  import 'swiper/css/pagination'
-
-  import './style.css'
-
-  // import required modules
-  import { Pagination } from 'swiper'
-
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      return {
-        modules: [Pagination],
-      }
-    },
+export default {
+  components: {
+    Footer
   }
+}
 </script>
